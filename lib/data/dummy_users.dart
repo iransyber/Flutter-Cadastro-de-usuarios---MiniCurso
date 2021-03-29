d48b805b432f92792809c63b0397af04545f7621
Iran Junior <iranjunior@solucaosistemas.net>
2021-03-29 18:17:50 -0300

    Primeiro commit final

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,34 @@
+import 'package:listatarefas/models/user.dart';
+
+const DUMMY_USER = {
+  '1': const User(
+      id: '1',
+      name: 'Joao maria',
+      email: 'joao@contoso.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'maria jose',
+      email: 'mariajose@contoso.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Euclides Lucio',
+      email: 'euclides@contoso.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Jose Toledo',
+      email: 'toledo@contoso.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png'),
+  '5': const User(
+      id: '5',
+      name: 'maria aparecida',
+      email: 'aparecida@contoso.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_960_720.png')
+};
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:listatarefas/provider/users.dart';
+import 'package:listatarefas/routes/app_routes.dart';
+import 'package:listatarefas/views/user_form.dart';
+import 'package:listatarefas/views/user_list.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Users(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          AppRoutes.HOME: (_) => UserList(),
+          AppRoutes.USER_FORM: (_) => UserForm()
+        },
+      ),
+    );
+  }
+}
